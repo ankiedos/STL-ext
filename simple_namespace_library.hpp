dae5317da1f6be8918f71bd3d7767e81699fd566
ankiedos <70840444+ankiedos@users.noreply.github.com>
2021-03-09 16:25:29 +0100

    Corrected indentation and got rid of using namespace and added #include<cmath>.

--- simple_namespace_library.hpp
+++ simple_namespace_library.hpp
@@ -10,53 +10,52 @@ A simple namespace library
 
 #include <iostream>
 #include <concepts>
-using namespace std;
-using namespace std::ranges;
+#include <cmath>
 namespace concepts
 {
-        namespace numberProperties
-        {
-                auto evn = [ ](auto m) { return m % 2 == 0 };
-                auto odd = [ ](auto n) { return n % 2 == 1 };
-                auto ngt = [ ] (auto a) {(a/-1) > 0};
-                auto pst  = [ ] (auto a) {(a/-1) < 0};
-                auto intgr = [ ] (auto a) {round(a) == a};
-                auto flt = [ ] (auto a) {round(a) != a};
-                auto dv3 = [ ] (auto a) {(a % 3) == 0};
-                auto cngr_mod [ ] (auto a, auto b, auto c) {a % c == b % c};
-                auto dvdn [ ] (auto a, auto b) {a % b == 0};
-        }
+    namespace numberProperties
+    {
+        auto evn = [](auto m) { return m % 2 == 0 };
+        auto odd = [](auto n) { return n % 2 == 1 };
+        auto ngt = [] (auto a) {(a/-1) > 0};
+        auto pst  = [] (auto a) {(a/-1) < 0};
+        auto intgr = [] (auto a) {std::round(a) == a};
+        auto flt = [] (auto a) {std::round(a) != a};
+        auto dv3 = [] (auto a) {(a % 3) == 0};
+        auto cngr_mod = [] (auto a, auto b, auto c) {a % c == b % c};
+        auto dvdn = [] (auto a, auto b) {a % b == 0};
+    }
 }
 
 namespace elementaryArithmetic
 {
-        auto power = [ ](auto a, auto e)
+    auto power = [](auto a, auto e)
+    {
+        for(int i=0; i <= e/2; i++)
         {
-                for(int i=0; i <= e/2; i++)
-                {
-                        a*=a;
-                }
-                return a;
+            a*=a;
         }
+        return a;
+    }
 }
 
 namespace out
 {
-        ostream& operator<<(ostream& wektor, range auto&& zakres) requires (!is_convertible_v<decltype(zakres), string>)
+    ostream& operator<<(ostream& wektor, std::ranges::range auto&& zakres) requires (!is_convertible_v<decltype(zakres), string>)
+    {
+        auto obecny = std::begin(zakres);
+
+        if(obecny == std::end(zakres))
         {
-                auto obecny = begin(zakres);
-
-                if (obecny == end(zakres))
-                {
-                        return wektor << "||";
-                }
-
-                wektor << '|' << *obecny;
-                while (++obecny != end(zakres))
-                {
-                    wektor << ',' << *obecny;
-                }
-
-               return wektor << '|';
+            return wektor << "||";
         }
+
+        wektor << '|' << *obecny;
+        while(++obecny != std::end(zakres))
+        {
+            wektor << ',' << *obecny;
+        }
+
+        return wektor << '|';
+    }
 }
