194a84574a49d8f2686c828279caefff429718d6
ankiedos <70840444+ankiedos@users.noreply.github.com>
2021-03-09 16:45:23 +0100

    Prepared for publishing.

--- temp_lib.hpp
+++ temp_lib.hpp
@@ -8,22 +8,24 @@
 A simple namespace library
 */
 
-#include <iostream>
-#include <concepts>
-#include <cmath>
+#include<iostream>
+#include<concepts>
+#include<cmath>
+#include<ranges>
+
 namespace concepts
 {
     namespace numberProperties
     {
-        auto evn = [](auto m) { return m % 2 == 0 };
-        auto odd = [](auto n) { return n % 2 == 1 };
-        auto ngt = [] (auto a) {(a/-1) > 0};
-        auto pst  = [] (auto a) {(a/-1) < 0};
-        auto intgr = [] (auto a) {std::round(a) == a};
-        auto flt = [] (auto a) {std::round(a) != a};
-        auto dv3 = [] (auto a) {(a % 3) == 0};
-        auto cngr_mod = [] (auto a, auto b, auto c) {a % c == b % c};
-        auto dvdn = [] (auto a, auto b) {a % b == 0};
+        auto evn = [](auto m)                       { return m % 2 == 0 };
+        auto odd = [](auto n)                       { return n % 2 == 1 };
+        auto ngt = [](auto a)                       { return (a / -1) > 0 };
+        auto pst  = [](auto a)                      { return (a / -1) < 0 };
+        auto intgr = [](auto a)                     { return std::round(a) == a };
+        auto flt = [](auto a)                       { return std::round(a) != a };
+        auto dv3 = [](auto a)                       { return (a % 3) == 0 };
+        auto cngr_mod = [](auto a, auto b, auto c)  { return (a % c) == (b % c) };
+        auto dvdn = [](auto a, auto b)              { return a % b == 0 };
     }
 }
 
@@ -31,9 +33,9 @@ namespace elementaryArithmetic
 {
     auto power = [](auto a, auto e)
     {
-        for(int i=0; i <= e/2; i++)
+        for(int i = 0; i <= e / 2; i++)
         {
-            a*=a;
+            a *= a;
         }
         return a;
     }
@@ -41,7 +43,7 @@ namespace elementaryArithmetic
 
 namespace out
 {
-    ostream& operator<<(ostream& vector, std::ranges::range auto&& range) requires (!is_convertible_v<decltype(range), std::string>)
+    std::ostream& operator<<(std::ostream& vector, std::ranges::range auto&& range) requires (!is_convertible_v<decltype(range), std::string>)
     {
         auto current = std::begin(range);
 
